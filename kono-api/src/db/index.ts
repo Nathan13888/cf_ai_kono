1fd16c20652f783a3b937967c971d9dc67b0d406
Justin Chung <20733699+justin13888@users.noreply.github.com>
2025-05-19 00:50:26 -0400

    kono-api: Fix types

--- kono-api/src/db/index.ts
+++ kono-api/src/db/index.ts
@@ -1,13 +1,26 @@
-import { drizzle } from 'drizzle-orm/d1';
+import { drizzle, DrizzleD1Database } from "drizzle-orm/d1";
+import { createMiddleware } from "hono/factory";
 
-export interface Env {
-    DB: D1Database;
+interface DbBindings {
+  db: DrizzleD1Database<Record<string, never>> & {
+    $client: D1Database;
+  };
 }
 
-// TODO: might break. use zod for runtime validation
-const env = process.env as unknown as Env;
+// export const db = await drizzle(c.env.DB);
 
-export const db = drizzle(env.DB);
+export const getDb = (d: D1Database) => {
+  return drizzle(d);
+};
+
+export const dbMiddleware = createMiddleware<{
+  Bindings: CloudflareBindings;
+  Variables: DbBindings;
+}>(async (c, next) => {
+  const db = await getDb(c.env.DB);
+  c.set("db", db);
+  await next();
+});
 
 // export default {
 //   async fetch(request: Request, env: Env) {
