5c1ca2160c69ee0af711caf4ef2a5d8931b43dfe
Justin Chung <20733699+justin13888@users.noreply.github.com>
2025-05-21 01:13:44 -0400

    Add routing

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    react(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
