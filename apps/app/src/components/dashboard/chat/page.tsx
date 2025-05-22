a0221c360f965eab287a9cda347a1b15cf8b60e4
Justin Chung <20733699+justin13888@users.noreply.github.com>
2025-05-22 15:50:52 -0400

    Fix typo

--- apps/app/src/components/dashboard/chat/page.tsx
+++ apps/app/src/components/dashboard/chat/page.tsx
@@ -112,7 +112,7 @@ export default function ChatInference() {
           
           {/* Right item */}
           {
-            isLoggedIn ? (
+            isLoggedIn() ? (
               <ProfileButton />
             ) : (
               <Button
