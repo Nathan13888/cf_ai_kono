a3d532079187eca31d485e810394b5f3d35d4d38
Nathan <29968201+Nathan13888@users.noreply.github.com>
2025-07-04 19:52:11 -0400

    fix web template
    Signed-off-by: Nathan <29968201+Nathan13888@users.noreply.github.com>

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,36 +1,26 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import Spotlight from "./Spotlight";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+declare module "@tanstack/react-router" {
+    interface Register {
+        router: typeof router;
+    }
 }
 
 // Render the app
 const root_dashboard = document.getElementById("root");
 if (root_dashboard) {
-  ReactDOM.createRoot(root_dashboard as HTMLElement).render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
-  );
-}
-
-const root_spotlight = document.getElementById("root-spotlight");
-if (root_spotlight) {
-  ReactDOM.createRoot(root_spotlight as HTMLElement).render(
-    <StrictMode>
-      <Spotlight />
-    </StrictMode>,
-  );
+    ReactDOM.createRoot(root_dashboard as HTMLElement).render(
+        <StrictMode>
+            <RouterProvider router={router} />
+        </StrictMode>,
+    );
 }
