c18b2bb2755d58fd1f3167fc63643948bd233ce7
Nathan <29968201+Nathan13888@users.noreply.github.com>
2025-07-25 02:59:48 -0400

    ops: deployment
    Signed-off-by: Nathan <29968201+Nathan13888@users.noreply.github.com>

--- apps/api/src/bindings.ts
+++ apps/api/src/bindings.ts
@@ -1,6 +1,8 @@
 import type { ChatDurableObject } from "./objects/chat";
 
+// export type Bindings = CloudflareBindings;
 export interface Bindings
     extends Omit<CloudflareBindings, "CHAT_DURABLE_OBJECT"> {
+    DB: D1Database;
     CHAT_DURABLE_OBJECT: DurableObjectNamespace<ChatDurableObject>;
 }
